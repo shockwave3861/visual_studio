47da2835a489ac6bd2637b4481390f9f3d43c628
sidev7 <sidev7@adorpower.com>
2022-02-01 12:08:44 +0530

    time calculation between object detected and image capture

--- VScode/Project Harmony/main.cpp
+++ VScode/Project Harmony/main.cpp
@@ -1,22 +1,31 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <string>
 #include <iostream> 
 #include <winsock2.h>
+#include <pylon/PylonIncludes.h>
+#include <ctime>
 
+using namespace Pylon;
 using namespace std;
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
 
+
 int main()//int argc, char* argv[])
 {
+	float speed_limit = 5;
+
 	WSADATA wsa;
 	SOCKET s;
 	struct sockaddr_in server;
 	char server_reply[200];
 	int recv_size;
+
+	PylonInitialize();
 
 	puts("\nInitialising Winsock...");
 
@@ -68,63 +77,94 @@ int main()//int argc, char* argv[])
 		//printf("%c", event_msg[0]);
 
 		string zone_type = event_msg.substr(0, event_msg.find(';'));
-		cout << "zone_type	: " << zone_type << endl;
+		//cout << "zone_type	: " << zone_type << endl;
 
 		if (zone_type == "MZ")
 		{
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string time_stamp = event_msg.substr(0, event_msg.find(';'));
-			cout << "time_stamp	: " << time_stamp << endl;
+			//cout << "time_stamp	: " << time_stamp << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string zone_number = event_msg.substr(0, event_msg.find(';'));
-			cout << "zone_number	: " << zone_number << endl;
+			//cout << "zone_number	: " << zone_number << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string speed = event_msg.substr(0, event_msg.find(';'));
 			//cout << "speed		: " << speed << endl;
 			float f_speed = stof(speed);
-			cout << "speed		: " << f_speed << endl;
+			//cout << "speed		: " << f_speed << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string object_class = event_msg.substr(0, event_msg.find(';'));
-			cout << "object_class	: " << object_class << endl;
+			//cout << "object_class	: " << object_class << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string direction = event_msg.substr(0, event_msg.find(';'));
-			cout << "direction	: " << direction << endl;
+			//cout << "direction	: " << direction << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string system_state = event_msg.substr(0, event_msg.find(';'));
-			cout << "system_state	: " << system_state << endl;
+			//cout << "system_state	: " << system_state << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string output_number = event_msg.substr(0, event_msg.find(';'));
-			cout << "output_number	: " << output_number << endl;
+			//cout << "output_number	: " << output_number << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string phase_number = event_msg.substr(0, event_msg.find(';'));
-			cout << "phase_number	: " << phase_number << endl;
+			//cout << "phase_number	: " << phase_number << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string object_id = event_msg.substr(0, event_msg.find(';'));
-			cout << "object_id	: " << object_id << endl;
+			//cout << "object_id	: " << object_id << endl;
 
 			event_msg.erase(0, event_msg.find(';') + 1);
 			//cout << event_msg << endl;
 			string eta = event_msg.substr(0, event_msg.find('\n'));
-			cout << "eta		: " << eta << endl;
+			//cout << "eta		: " << eta << endl;
 
-			
+			if (f_speed > speed_limit)
+			{
+				//cout << "********** Taking SS **********" << endl;
+				try
+				{
+					CGrabResultPtr ptrGrabResult;
+					CInstantCamera Camera(CTlFactory::GetInstance().CreateFirstDevice());
+
+					if (Camera.GrabOne(1000, ptrGrabResult))
+					{	
+						string png_name = time_stamp + ".png";
+						char* image_name = &png_name[0];
+
+						time_t now = time(0);
+						char* dt = ctime(&now);
+						cout << "Before : " << dt << endl;
+
+						CImagePersistence::Save(ImageFileFormat_Png, image_name, ptrGrabResult);
+						
+						now = time(0);
+						dt = ctime(&now);
+						cout << "After : " << dt << endl;
+					}
+				}
+				catch (const GenericException& e)
+				{
+					cerr << "Could not grab an image: " << endl
+						<< e.GetDescription() << endl;
+				}
+			}
+
+
 		}
 
 
---
+++ VScode/TCPclient/Program.cs
@@ -0,0 +1,58 @@
+﻿/*       Client Program      */
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class clnt
+{
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect("172.16.16.142", 62150);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+
+            //Console.Write("Enter the string to be transmitted : ");
+            //String str = Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+            //ASCIIEncoding asen = new ASCIIEncoding();
+            //byte[] ba = asen.GetBytes(str);
+            //Console.WriteLine("Transmitting.....");
+            //stm.Write(ba, 0, ba.Length);
+            //byte[] bb = new byte[100];
+            //int k = stm.Read(bb, 0, 100);
+            //for (int i = 0; i < k; i++)
+            //    Console.Write(Convert.ToChar(bb[i]));
+
+            while (true)
+            {
+                Byte[] data = new Byte[256];
+                String responseData = String.Empty;
+
+                Int32 bytes = stm.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+            }
+            tcpclnt.Close();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
